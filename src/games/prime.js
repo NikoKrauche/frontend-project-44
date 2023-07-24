90f627e11851e9f1ca1b7c8b611212ce8ead72cd
NikoKrauche <K0lan@ya.ru>
2023-07-24 21:13:44 +0300

    modify prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,16 +4,17 @@ import startEngine from '../index.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  for (let i = 2; i < number; i += 1) {
+  if (number < 2) return false;
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) return false;
   }
   return true;
 };
 
 const gerenerateRound = () => {
-  const question = getRandomNumber(2, 51);
-  const correctAnswer = isPrime(question) ? 'yes' : 'no';
-  return [question, correctAnswer];
+  const question = getRandomNumber(1, 51);
+  const answer = isPrime(question) ? 'yes' : 'no';
+  return [question, answer];
 };
 
 const startGame = () => {
