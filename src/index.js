ecb0eb2c97e044549222ba5a44f40aa1a1cecaba
NikoKrauche <K0lan@ya.ru>
2023-07-24 21:29:53 +0300

    modify index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import readlineSync from 'readline-sync';
-import { welcomeUser, name } from './cli.js';
 
 const roundsCount = 3;
 
 const startEngine = (description, gerenerateRound) => {
-  console.log(welcomeUser);
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
   console.log(description);
 
   for (let i = 0; i < roundsCount; i += 1) {
